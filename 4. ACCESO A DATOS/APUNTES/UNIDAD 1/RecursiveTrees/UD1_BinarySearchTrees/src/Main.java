abfa3a50e1dc46aaccbd4e1d6990e0b637388ea5
cris <cristina.cg9@hotmail.com>
2024-10-22 16:27:54 +0200

    Ejercicios de árboles

--- 4. ACCESO A DATOS/APUNTES/UNIDAD 1/RecursiveTrees/UD1_BinarySearchTrees/src/Main.java
+++ 4. ACCESO A DATOS/APUNTES/UNIDAD 1/RecursiveTrees/UD1_BinarySearchTrees/src/Main.java
@@ -12,6 +12,15 @@ public class Main {
 		tree.addNode(8);
 		tree.addNode(10);
 		
+		BST tree2 = new BST();
+		tree2.addNode(7);
+		tree2.addNode(2);
+		tree2.addNode(1);
+		tree2.addNode(5);
+		tree2.addNode(9);
+		tree2.addNode(8);
+		tree2.addNode(10);
+		
 		/*tree.preOrden();
 		System.out.println();
 		tree.postOrden();
@@ -19,12 +28,13 @@ public class Main {
 		tree.inOrden();*/
 		
 		// System.out.println(tree.isStrict());
-		
 		// System.out.println(tree.treeHeight());
-
+		// System.out.println(tree.maxValue());
 		// System.out.println(tree.countNodes());
-		System.out.println(tree.sum());
-		System.out.println(tree.sumPares());
+		System.out.println(tree.equal(tree2));
+		// System.out.println(tree.countLeaves());
+		// System.out.println(tree.sum());
+		// System.out.println(tree.sumPares());
 		
 	}
 
